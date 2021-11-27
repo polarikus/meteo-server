670a6dbeff4cbdbb00d4846e1c4b3a9f74d2099b
igordanilov <polarikus@yandex.ru>
2021-11-27 18:16:42 +0300

    V1.0

--- resources/views/report-meteo-sensor.blade.php
+++ resources/views/report-meteo-sensor.blade.php
@@ -21,7 +21,7 @@
                 let time = moment();
                 let lastOnline = moment(data.last_online.last_online);
                 lastOnline = moment(data.last_meteo_data[0].created_at);
-                let duration = moment.duration(end.diff(startTime));
+                let duration = moment.duration(lastOnline.diff(startTime));
                 console.log('Разница: ' + duration);
                 console.log('Сейчас: ' + time);
                 console.log('Был: ' + lastOnline);
